0f0866c8f8712eb929e6b7ebc02d7504446ad5b8
Shubham Yadav <shubh08am@gmail.com>
2023-09-06 16:24:28 +0530

    Update 974. Subarray Sums Divisible by K.cpp

--- 974. Subarray Sums Divisible by K.cpp
+++ 974. Subarray Sums Divisible by K.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-         //prefix sum and remainder 
-         unordered_map<int,int>rem;
-         int cnt=0;
-         for(int i=1;i<nums.size();i++) {
-             nums[i]+=nums[i-1];
-         }
-        //take remainder 
+        // Prefix sum and remainder property 
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int prefixSum = 0 , cnt =0 ;
         for(int i=0;i<nums.size();i++){
-            int remainder = nums[i]%k ; 
-            //-ve handle separately again add k and than take modulo
-            if(remainder<0){
-                int newRemainder =  (remainder + k) % k;  
-                // 0 handle separately take it's freq first than cnt || [-5] -> 1
-                if(newRemainder==0){
-                    rem[newRemainder]++;
-                  //  cout<<newRemainder << " "<<rem[newRemainder]<<"\n";
-                    cnt+=rem[newRemainder];
-                }
-             else{ 
-                cnt+=rem[newRemainder];
-               // cout<<newRemainder << " "<<rem[newRemainder]<<"\n";
-                 rem[newRemainder]++;
-             }
-            }
-            if(remainder>=0) {
-                // 0 handle separately take it's freq first than cnt
-                if(remainder==0){
-                    rem[remainder]++;
-                  //  cout<<remainder << " "<<rem[remainder]<<"\n";
-                    cnt+=rem[remainder];
-                }
-               else{
-                 cnt+=rem[remainder];
-                 // cout<<remainder << " "<<rem[remainder]<<"\n";
-                  rem[remainder]++;  
-               }             
-                }
+            //don't add all number -> Runtime can occur in this type of quest 
+            //better to add remainder if -ve handle it by adding k 
+            prefixSum = (prefixSum + (nums[i]%k) + k ) %k ;
+            cnt+=mp[prefixSum];
+            mp[prefixSum]++;
         }
-        
-         return cnt;
+    return cnt;
     }
 };

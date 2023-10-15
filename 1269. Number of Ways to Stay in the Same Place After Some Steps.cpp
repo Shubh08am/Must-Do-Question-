2ebe8b7ac58a7123f38553409157d96fd0c86252
Shubham Yadav <shubh08am@gmail.com>
2023-10-15 19:34:38 +0530

    Update 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp

--- 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
+++ 1269. Number of Ways to Stay in the Same Place After Some Steps.cpp
@@ -16,12 +16,47 @@ public:
         long S = solve(ind,steps-1,arrLen,dp)%mod ; 
     return dp[ind][steps] = (R+L+S)%mod ;
     }
+    int tab(int steps,int arrLen){
+        int move = min(steps/2+1,arrLen);
+        vector<vector<int>>dp(move+1,vector<int>(steps+1,0)) ;
+        dp[0][0]=1; 
+            for(int step=1;step<=steps;step++){
+                for(int ind=move-1;ind>=0;ind--){
+                long R=0,L=0,S=0;
+                S = dp[ind][step-1] ; 
+                if(ind>0) L = dp[ind-1][step-1] ; 
+                if(ind<move) R= dp[ind+1][step-1] ;
+
+            dp[ind][step]=(L+R+S)%mod; 
+            }
+        }
+    return dp[0][steps];
+    }
+        int tabS(int steps,int arrLen){
+        int move = min(steps/2+1,arrLen);
+        vector<int>prev(move+1,0),curr(move+1,0);
+        prev[0]=1; 
+            for(int step=1;step<=steps;step++){
+                for(int ind=move-1;ind>=0;ind--){
+                long R=0,L=0,S=0;
+                S = prev[ind] ; 
+                if(ind>0) L = prev[ind-1] ; 
+                if(ind<move-1) R= prev[ind+1] ;
+
+            curr[ind]=(L+R+S)%mod; 
+            }
+            prev=curr;
+        }
+    return prev[0];
+    }
     int numWays(int steps, int arrLen) {
        // WHY THOSE WHO CAN'T  REMEMBER THE PAST ARE CONDEMNED TO REMEMBER IT :) 
         int move = min(steps/2+1,arrLen);
         //OBSERVATION :- WE CAN MOVE STEPS/2+1 TIMES MAX RIGHT OR LEFT 
         // JUST IGNORING ARRLEN FACT 
         vector<vector<int>>dp(move,vector<int>(steps+1,-1));
-        return solve(0,steps,move,dp);
+      //  return solve(0,steps,move,dp);
+     // return tab(steps,arrLen);
+     return tabS(steps,arrLen);
     }
 };

387c7be9958905a5d5ff18c6b4fe67318084a11c
Shubham Yadav <shubh08am@gmail.com>
2023-10-21 19:39:33 +0530

    Update 189. Rotate Array.cpp

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -10,13 +10,24 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        //1-> REVERSE FIRST N-K ELEMENTS 
-        //2-> REVERSE LAST K ELEMENTS 
+      /*
+        //1->REVERSE FIRST N-K ELEMENTS I.E N-K-1
+        //2->REVERSE LAST K ELEMENTS I.E FROM N-KTH TO N-1TH
         //3->REVERSE ALL THE ELEMENTS 
         int n = nums.size(); 
         k%=n;
         reverse(nums,0,n-k-1);
         reverse(nums,n-k,n-1); 
         reverse(nums,0,n-1);
+        */
+
+        //1->REVERSE ALL THE ELEMENTS
+        //2->REVERSE FIRST K ELEMENTS I.E K-1
+        //3->REVERSE REMAINING ELEMENTS I.E FROM KTH TO N-1TH
+        int n = nums.size(); 
+        k%=n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1); 
+        reverse(nums,k,n-1);
     }
 };
